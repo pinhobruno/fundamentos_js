2fafa784b0e4c45f2c832414bb25ea6ffab50844
Bruno Nobrega Pinho <pinhobruno@hotmail.com>
2021-10-01 14:02:27 -0300

    Exemplos da Aula 1

---
+++ type-string.js
@@ -0,0 +1,9 @@
+const texto1 = 'Olá, mundo!';
+const texto2 = 'Olá, mundo!';
+const senha = 'senhaSuperSegura456!';
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+const inputMin = input.toLowerCase();
+console.log(cidade + " - " + inputMin)
+console.log(cidade == inputMin); //false
